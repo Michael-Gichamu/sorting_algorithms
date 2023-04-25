cc77f65bcc7bda80cf0482bb71ba2aecff7ad401
Michael-Gichamu <michaelmatere606@gmail.com>
2023-04-25 15:49:50 -0700

    Quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,7 +1,4 @@
 #include "sort.h"
-void quicksort_recursion(int array[], int low, int high, size_t size);
-void swap(int *array, int x, int y);
-int partition(int *array, int low, int high, size_t size);
 
 /**
  * quick_sort - sorts an array of integers in ascending order,
@@ -21,6 +18,7 @@ void quick_sort(int *array, size_t size)
  * @array: The array.
  * @low: Lowest index.
  * @high: Highest index.
+ * @size: size of array.
  *
  * Return: Always(void).
  */
@@ -29,6 +27,7 @@ void quicksort_recursion(int array[], int low, int high, size_t size)
 	if (low < high)
 	{
 		int pivot_index = partition(array, low, high, size);
+
 		quicksort_recursion(array, low, pivot_index - 1, size);
 		quicksort_recursion(array, pivot_index + 1, high, size);
 	}
@@ -39,32 +38,33 @@ void quicksort_recursion(int array[], int low, int high, size_t size)
  * @array: array to partition.
  * @low: Lowest index.
  * @high: Highest index.
+ * @size: size of array.
  *
  * Return: Always(int)
  */
 int partition(int *array, int low, int high, size_t size)
 {
 	int pivot_value = array[high];
-	int j, i = low;
+	int i = low;
+	int j = low;
 
 	for (j = low; j < high; j++)
 	{
-		if (array[j] <= pivot_value)
+		if (array[j] < pivot_value)
 		{
 			swap(array, i, j);
 			i++;
-			print_array(array, size);
 		}
 	}
 	swap(array, i, high);
 	print_array(array, size);
 
-	return i;
+	return (i);
 }
 
 /**
  * swap - swap two items.
- * @array - The array.
+ * @array: The array.
  * @x: first element.
  * @y: second element.
  *
@@ -73,6 +73,7 @@ int partition(int *array, int low, int high, size_t size)
 void swap(int *array, int x, int y)
 {
 	int temp = array[x];
+
 	array[x] = array[y];
 	array[y] = temp;
 }
